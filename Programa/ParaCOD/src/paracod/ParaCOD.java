e5fe231951f98c58d078df505ac1ea20b8792f7e
Cristian Moreira <cmoreiraalen@danielcastelao.org>
2022-11-18 09:51:47 +0100

    1ra commit

---
+++ Programa/ParaCOD/src/paracod/ParaCOD.java
@@ -0,0 +1,27 @@
+
+package paracod;
+
+
+public class ParaCOD {
+
+    
+    public static void main(String[] args) {
+        Funciones obj1 = new Funciones();
+            obj1.mostrarMenu();
+        Funciones obj2 = new Funciones();
+            obj2.introducirOpcion();
+        Funciones obj3 = new Funciones();
+            obj3.introducirNumero();
+        Funciones obj4 = new Funciones();
+            obj4.calcularAreaCircunferencia(1, 7);
+        Funciones obj5 = new Funciones();
+                    obj5.calcularAreaCuadrado(5);
+        Funciones obj6 = new Funciones();
+            obj6.calcularAreaTriangulo(25, 5);
+        Funciones obj7 = new Funciones();            
+            obj7.mostrarArea(0);
+                            
+
+    
+}
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # examenDAM
 Para utilizarlo en el examen
+##Cristian Moreira Examen COD
+
+1--> Clonamos el repositorio: https://github.com/damiancastelao/examenDAM.git
+	Lo clonamos utilizando: *git clone* https://github.com/damiancastelao/examenDAM.git
+	
+2-->Creo un repositorio en GitHub: https://github.com/cristianmoreiraa/ExamenCOD.git
+
+3-->Con el comando mkdir desde la consola creo una nueva carpeta llamada "Programa", en la cual meteré el código.
+
+4-->*git init*: para iniciar el repositorio Git
+
+5-->*git add*. Para que se añada todo los ficheros que tengo en 'examenDAM'
+
+6-->Hago la primera commit "1ra commit" usando: *git commit -m "1ra commit"*
+
